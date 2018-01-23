670458e66bf5e3f4faed74f58a001b9a40d3a9de
Mikyg03 <cannavomichele@live.it>
2018-01-24 00:15:37 +0100

    1.3.0
    Aggiunta Licenza

--- Sorgenti/My_String.h
+++ Sorgenti/My_String.h
@@ -1,3 +1,20 @@
+/************************************************************************************************************
+ * This file is part of My_String.
+ *
+ * My_String is free software: you can redistribute it and/or moify
+ * it under the terms of the GNU Lesser General Public License v3.0  as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * My_String is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License v3.0  for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License v3.0 
+ * along with My_String. If not, see <http://www.gnu.org/licenses/>.
+ ***********************************************************************************************************/
+
 /**
  *  \file MyString.h
  *  \brief Header per la gestione della libreria MYString
--- Sorgenti/strrevv.c
+++ Sorgenti/strrevv.c
@@ -1,18 +1,18 @@
 /************************************************************************************************************
- * ThdimStrings file is part of Nome-Programma.
+ * This file is part of My_String.
  *
- * Nome-Programma is free software: you can redistribute it and/or moify
- * it under the terms of the GNU General Public License as published by
+ * My_String is free software: you can redistribute it and/or moify
+ * it under the terms of the GNU Lesser General Public License v3.0  as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Nome-Programma is distributed in the hope that it will be useful,
+ * My_String is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License v3.0  for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Nome-Programma. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License v3.0 
+ * along with My_String. If not, see <http://www.gnu.org/licenses/>.
  ***********************************************************************************************************/
 #include "My_String.h"
 
